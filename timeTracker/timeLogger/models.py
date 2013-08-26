9a890335c952956226b970810a9cc2236ba25120
pbrogan <pbrogan12@gmail.com>
2013-08-26 17:31:59 -0400

    Updated models no longer needs ends time

--- timeTracker/timeLogger/models.py
+++ timeTracker/timeLogger/models.py
@@ -10,9 +10,8 @@ class Activity(models.Model):
 class logActivity(models.Model):
     name = models.ForeignKey('Activity')
     date = models.DateField()
-    startTime = models.TimeField()
-    endTime = models.TimeField()
-    notes = models.TextField(max_length=20)
+    time = models.CharField(max_length=20)
+    notes = models.TextField()
     
     def __unicode__(self):
         return u'%s %s' % (self.name, self.notes)
