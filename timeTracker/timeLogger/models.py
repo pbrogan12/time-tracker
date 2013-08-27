4033fa582b43f0243bf6746564e4eaaf354e37a0
pbrogan <pbrogan12@gmail.com>
2013-08-27 12:04:52 -0400

    Changed time field to int field.

--- timeTracker/timeLogger/models.py
+++ timeTracker/timeLogger/models.py
@@ -10,7 +10,7 @@ class Activity(models.Model):
 class logActivity(models.Model):
     name = models.ForeignKey('Activity')
     date = models.DateField()
-    time = models.CharField(max_length=20)
+    time = models.IntegerField()
     notes = models.TextField()
     
     def __unicode__(self):
