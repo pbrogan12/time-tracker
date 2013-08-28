6408d877bc5e31fdf477c43bab45c65b11ea0a91
pbrogan <pbrogan12@gmail.com>
2013-08-28 11:38:17 -0400

    Reset virtual env.

--- bin/activate.fish
+++ bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pbrogan/timeTracker"
+set -gx VIRTUAL_ENV "/home/pbrogan/time-tracker"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
