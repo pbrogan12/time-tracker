6c138b0dba767588e5e0932351a5d53417f188c8
pbrogan <pbrogan12@gmail.com>
2013-08-31 12:22:59 -0400

    Made admin page nicer

--- timeTracker/timeLogger/admin.py
+++ timeTracker/timeLogger/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from timeLogger.models import Activity, logActivity, dailySummary
 
+def username(obj):
+    return u'%s' % (obj.account.user)
+
+class dailySummaryAdmin(admin.ModelAdmin):
+    list_display = (username,'date','rating')
+    ordering = ('rating','-date')
+
 admin.site.register(Activity)
 admin.site.register(logActivity)
-admin.site.register(dailySummary)
+admin.site.register(dailySummary, dailySummaryAdmin)
