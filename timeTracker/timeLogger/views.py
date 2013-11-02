3c0ab5c8405b1ebd176f0abb250bb9a599e14528
pbrogan <pbrogan12@gmail.com>
2013-11-02 11:52:20 -0400

    fixes #2

--- timeTracker/timeLogger/views.py
+++ timeTracker/timeLogger/views.py
@@ -8,7 +8,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 import datetime
 
-
 @login_required(login_url='/accounts/signin')
 def addLog(request, actId):
     accountId = MyProfile.objects.get(user_id=request.user.id)
